b935c04d577a20850ba677bdecd99366594618c7
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-12-25 17:11:28 -0300

    feat: add appeal stylization

--- modules/Appeal/index.tsx
+++ modules/Appeal/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "../../components/Button";
 import { useLocale } from "../../hooks/locale";
 import styles from "../../styles/Appeal.module.scss";
 
@@ -7,18 +8,23 @@ const Appeal: React.FC = () => {
 
   return (
     <section itemScope className={styles.appeal}>
-      <hgroup itemProp="appeal">
-        <h1 itemProp="headline">{strings.blocks.appeal.title}</h1>
-        <p itemProp="message">{strings.blocks.appeal.subtitle}</p>
-      </hgroup>
+      <div className="appeal--content-container">
+        <hgroup itemProp="appeal">
+          <h5 itemProp="headline" className="regular-text">
+            {strings.blocks.appeal.title}
+          </h5>
 
-      <footer>
-        <p itemProp="description">{strings.blocks.appeal.description}</p>
+          <p itemProp="message">{strings.blocks.appeal.subtitle}</p>
+        </hgroup>
 
-        <button type="button" itemProp="contactButton">
-          {strings.blocks.appeal.action}
-        </button>
-      </footer>
+        <footer className="appeal--footer">
+          <p itemProp="description">{strings.blocks.appeal.description}</p>
+
+          <Button variant="secondary" itemProp="contactButton">
+            {strings.blocks.appeal.action}
+          </Button>
+        </footer>
+      </div>
     </section>
   );
 };
