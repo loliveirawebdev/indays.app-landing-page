50d1bda0911942cbf77dbf9e3a2b1bdc0bfbef12
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-12-25 15:27:02 -0300

    feat: add localizated social media icons alt

--- modules/Footer/components/SocialIcons.tsx
+++ modules/Footer/components/SocialIcons.tsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { useLocale } from "../../../hooks/locale";
 
 export const SocialIcons: React.FC = () => {
+  const { strings } = useLocale();
+
   return (
-    <menu itemScope itemProp="socialMediaLinks">
+    <menu itemScope itemProp="socialMediaLinks" className="footer--social-media-links">
       <li itemProp="socialMediaLink">
         <a href="#" itemProp="link">
-          <img src="#" alt="github" itemProp="icon" />
+          <img src="#" alt={strings.blocks.footer.socialMediaLinks.alts.github} itemProp="icon" />
         </a>
       </li>
 
       <li itemProp="socialMediaLink">
         <a href="#" itemProp="link">
-          <img src="#" alt="linkedin" itemProp="icon" />
+          <img src="#" alt={strings.blocks.footer.socialMediaLinks.alts.linkedin} itemProp="icon" />
         </a>
       </li>
     </menu>
