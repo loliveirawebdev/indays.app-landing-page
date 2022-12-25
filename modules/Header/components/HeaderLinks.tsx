ebe88a8e8d02ed3c4e8d714a7dbf52cc6fb6c5be
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-12-25 18:00:25 -0300

    feat: add header stylization

--- modules/Header/components/HeaderLinks.tsx
+++ modules/Header/components/HeaderLinks.tsx
@@ -2,17 +2,33 @@ import { v4 as uuid } from "uuid";
 
 interface Props {
   items: Header.MenuItem[];
+  isMobileCollapsed: boolean;
+  collapseMobile: () => void;
 }
 
-export const HeaderLinks: React.FC<Props> = ({ items }) => {
+export const HeaderLinks: React.FC<Props> = (props) => {
+  const { items, isMobileCollapsed, collapseMobile } = props;
+
+  const generateClassList = () => {
+    const collapsedClass = isMobileCollapsed ? "header--menu__collapsed" : "";
+    return `header--menu ${collapsedClass}`;
+  };
+
+  const goTo = (area: string) => {
+    return () => {
+      collapseMobile();
+      console.log(area);
+    };
+  };
+
   const renderMenuItems = (): React.ReactNode =>
     items.map((item) => {
       const uniqueKey = uuid();
-      const { label } = item;
+      const { label, action } = item;
 
       return (
         <li key={uniqueKey} itemProp="item">
-          <button type="button" itemProp="name">
+          <button type="button" itemProp="name" className="header--menu-item" onClick={goTo(action)}>
             {label}
           </button>
         </li>
@@ -20,8 +36,12 @@ export const HeaderLinks: React.FC<Props> = ({ items }) => {
     });
 
   return (
-    <menu itemScope itemType="https://schema.org/SiteNavigationElement">
-      {renderMenuItems()}
-    </menu>
+    <div className={generateClassList()}>
+      <div className="header--menu--backdrop" onClick={collapseMobile} />
+
+      <menu itemScope itemType="https://schema.org/SiteNavigationElement">
+        {renderMenuItems()}
+      </menu>
+    </div>
   );
 };
--- modules/Header/index.tsx
+++ modules/Header/index.tsx
@@ -8,10 +8,22 @@ const Header: React.FC = () => {
   const { strings } = useLocale();
   const menuLinks = buildHeaderMenuItems(strings.blocks.header.menu);
 
+  const [isMobileCollapsed, setIsMobileCollapsed] = React.useState(true);
+  const collapseMobile = () => setIsMobileCollapsed(true);
+  const openMobileMenu = () => setIsMobileCollapsed(false);
+
   return (
     <header itemScope itemProp="header" className={styles.header}>
-      <img src="/assets/logo.svg" alt="indays.app's Logo" itemProp="logo" />
-      <HeaderLinks items={menuLinks} />
+      <div className="header--content-container">
+        <img src="/assets/logo.svg" alt="indays.app's Logo" itemProp="logo" />
+        <span className="header--menu-separator" />
+
+        <button type="button" onClick={openMobileMenu} className="header--hamburger-menu">
+          <img src="/assets/hamburger-menu.svg" alt="Expand menu options" itemProp="showMenu" />
+        </button>
+
+        <HeaderLinks collapseMobile={collapseMobile} isMobileCollapsed={isMobileCollapsed} items={menuLinks} />
+      </div>
     </header>
   );
 };
