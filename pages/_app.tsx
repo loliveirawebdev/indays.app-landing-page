97346b40b474877d5d2a0b3c0ca50955194294cb
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-12-25 15:09:02 -0300

    feat: add open sans font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { useLocale } from "../hooks/locale";
+import { Open_Sans } from "@next/font/google";
+
 import "../styles/_globals.scss";
+const openSans = Open_Sans({ subsets: ["latin"] });
+const openSansStyle = `html { font-family: ${openSans.style.fontFamily}; }`;
 
 export default function App({ Component, pageProps }: AppProps) {
   const { strings } = useLocale();
@@ -13,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>{strings.page.title}</title>
         <meta name="title" content={strings.page.title} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style global>{openSansStyle}</style>
       </Head>
 
       <Component {...pageProps} />
