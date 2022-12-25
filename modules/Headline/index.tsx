1aa65235c748a2331029023797c634cc24e26f51
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-12-25 14:46:41 -0300

    feat: define headline scss module

--- modules/Headline/index.tsx
+++ modules/Headline/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useLocale } from "../../hooks/locale";
+import { renderCards } from "./helpers/renderCards";
+import styles from "../../styles/Headline.module.scss";
 import { renderParagraphs } from "../../helpers/renderParagraphs";
 import { buildHeadlineCards } from "./helpers/buildHeadlineCards";
-import { renderCards } from "./helpers/renderCards";
 
 const Headline: React.FC = () => {
   const { strings } = useLocale();
   const cards = buildHeadlineCards(strings.blocks.headline.cards);
 
   return (
-    <article itemScope itemProp="copywrite">
+    <article itemScope itemProp="copywrite" className={styles.headline}>
       <header itemScope itemProp="headline">
         <h1 itemProp="merit">{strings.blocks.headline.title}</h1>
         <h2 itemProp="merit">{strings.blocks.headline.subtitle}</h2>
