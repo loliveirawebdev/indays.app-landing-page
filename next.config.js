df6837cfac28d5357cb06ba8ef44e890874cd466
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-12-26 19:49:13 -0300

    chore: disablle strict mode

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
   i18n: {
     locales: ["en", "pt-BR"],
     defaultLocale: "pt-BR",
   },
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
