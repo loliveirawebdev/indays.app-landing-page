1b5c3c6c20295689977360daa9fce1a35ff94395
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-12-27 18:07:32 -0300

    feat: open form modal

--- modules/Contact/containers/ContactForm.tsx
+++ modules/Contact/containers/ContactForm.tsx
@@ -5,21 +5,34 @@ import { useLocale } from "../../../hooks/locale";
 import { useSectionsRefs } from "../../../hooks/refs";
 import { IS_FORM_CONTACT_SENT } from "../../../atoms/contact";
 import { useFormSubmit, useQuestions } from "../hooks/questions";
+import { usePageScroll } from "../../../hooks/pageScroll";
+import { Button } from "../../../components/Button";
 
 export const ContactForm: React.FC = () => {
   const [isFormSent] = useRecoilState(IS_FORM_CONTACT_SENT);
-  const containerClass = isFormSent ? `contact-form-container__is-sent` : "";
+  const [isFormOpen, setIsFormOpen] = React.useState(false);
 
-  const { config, inputRenderer } = useQuestions();
-  const onSubmit = useFormSubmit();
   const { strings } = useLocale();
-
   const { registerRef } = useSectionsRefs();
+  const { config, inputRenderer } = useQuestions();
+  const { blockScroll, unblockScroll } = usePageScroll();
+
+  const onSubmit = useFormSubmit();
   const ref = React.useRef<HTMLFormElement>(null);
 
   React.useEffect(() => {
     registerRef("contact-form", ref);
   }, [ref]);
+
+  const onOpenForm = () => {
+    blockScroll();
+    setIsFormOpen(true);
+  };
+
+  const onCloseForm = () => {
+    unblockScroll();
+    setIsFormOpen(false);
+  };
 
   const renderForm = () => {
     return config.map((question) => {
@@ -34,16 +47,41 @@ export const ContactForm: React.FC = () => {
     });
   };
 
+  const generateContainerClasses = () => {
+    const classList = ["contact-form--container"];
+
+    if (isFormSent) classList.push("contact-form--container__is-sent");
+
+    if (isFormOpen) classList.push("contact-form--container__is-open");
+    else classList.push("contact-form--container__is-closed");
+
+    return classList.join(" ");
+  };
+
   return (
-    <div className={containerClass}>
-      <div className="contact-form-sent">
+    <div className={generateContainerClasses()}>
+      <div className="contact-form--header">
+        <Button variant="tertiary" type="button" onClick={onCloseForm}>
+          {strings.blocks.contact.form.actions.back}
+        </Button>
+      </div>
+
+      <div className="contact-form--open-helper">
+        <div className="open-helper--backdrop">
+          <Button variant="secondary" type="button" onClick={onOpenForm}>
+            {strings.blocks.contact.form.actions.open}
+          </Button>
+        </div>
+      </div>
+
+      <div className="contact-form--sent">
         <p>{strings.blocks.contact.form.success.title}</p>
         <p>{strings.blocks.contact.form.success.description}</p>
       </div>
 
       <form ref={ref} onSubmit={onSubmit} itemScope itemProp="contactForm" className="contact-form">
         {renderForm()}
-        <button type="submit">oi</button>
+        <Button type="submit">{strings.blocks.contact.form.actions.send}</Button>
       </form>
     </div>
   );
