4cfefd32483689f2c0ec16feb14eefc22e42e0cc
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-12-27 16:30:43 -0300

    feat: add react-toastify library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { RecoilRoot } from "recoil";
 import type { AppProps } from "next/app";
 import { useLocale } from "../hooks/locale";
 import { Open_Sans } from "@next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 import "../styles/_globals.scss";
 const openSans = Open_Sans({ subsets: ["latin"] });
@@ -22,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <RecoilRoot>
+        <ToastContainer />
         <Component {...pageProps} />
       </RecoilRoot>
     </React.Fragment>
