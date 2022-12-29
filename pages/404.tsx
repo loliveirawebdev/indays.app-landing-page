ed83ba19789e80493865018f7e962a3138f9e1b7
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2022-12-29 03:36:43 -0300

    feat: add custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,17 @@
+import { Logo } from "../components/Logo";
+import { useLocale } from "../hooks/locale";
+import style from "../styles/ErrorMessage.module.scss";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  const { strings } = useLocale();
+
+  return (
+    <div className={style.errorMessage}>
+      <div className="error-message--inner-container">
+        <Logo />
+        <p>{strings.page["not-found"].message}</p>
+        <a href="https://indays.app">{strings.page["not-found"].action}</a>
+      </div>
+    </div>
+  );
 }
