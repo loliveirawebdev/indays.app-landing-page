cdcfab90305d0c283560687500204703d665d713
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2022-12-31 00:15:49 -0300

    feat: add header button

--- modules/Headline/index.tsx
+++ modules/Headline/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { useSetRecoilState } from "recoil";
 import { useLocale } from "../../hooks/locale";
+import { Button } from "../../components/Button";
 import { renderCards } from "./helpers/renderCards";
 import styles from "../../styles/Headline.module.scss";
+import { IS_FORM_CONTACT_OPEN } from "../../atoms/contact";
 import { renderParagraphs } from "../../helpers/renderParagraphs";
 import { buildHeadlineCards } from "./helpers/buildHeadlineCards";
 
 const Headline: React.FC = () => {
   const { strings } = useLocale();
+  const setIsFormOpen = useSetRecoilState(IS_FORM_CONTACT_OPEN);
   const cards = buildHeadlineCards(strings.blocks.headline.cards);
+  const openForm = () => setIsFormOpen(true);
 
   return (
     <article itemScope itemProp="copywrite" className={styles.headline}>
@@ -20,6 +25,10 @@ const Headline: React.FC = () => {
         <header itemScope itemProp="headline">
           <h1 itemProp="merit">{strings.blocks.headline.title}</h1>
           <h2 itemProp="merit">{strings.blocks.headline.subtitle}</h2>
+
+          <Button onClick={openForm} variant="primary" type="button" itemProp="form">
+            {strings.blocks.headline.action}
+          </Button>
         </header>
 
         <section itemScope itemProp="content" className="headline--content">
