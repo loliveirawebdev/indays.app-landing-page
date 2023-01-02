afe63ece024f5e5a6c07079411b092db60b15c37
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-01-02 18:28:01 -0300

    fix: move dynamic tags from _app and _document files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Head from "next/head";
 import { RecoilRoot } from "recoil";
 import type { AppProps } from "next/app";
-import { useLocale } from "../hooks/locale";
 import { Open_Sans } from "@next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
@@ -11,13 +10,9 @@ import "../styles/_globals.scss";
 const openSans = Open_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { strings } = useLocale();
-
   return (
     <React.Fragment>
       <Head>
-        <title>{strings.page.title}</title>
-        <meta name="title" content={strings.page.title} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,10 @@
-import { useUncontextualizedLocale } from "../hooks/locale";
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentProps } from "next/document";
 
 export default function CustomDocument(props: DocumentProps) {
-  const { strings } = useUncontextualizedLocale(props.locale!);
-
   return (
     <Html lang={props.locale}>
       <Head>
         <meta name="copyright" content="Simbiose Ventures" />
-        <meta name="pagename" content={strings.page.title} />
-        <meta name="keywords" content={strings.page.keywords} />
-        <meta name="subject" content={strings.page.description} />
-        <meta name="subtitle" content={strings.page.description} />
-        <meta name="description" content={strings.page.description} />
         <meta name="reply-to" content="lucas.oliveira@simbioseventures.com" />
 
         <meta charSet="UTF-8" />
@@ -25,19 +17,8 @@ export default function CustomDocument(props: DocumentProps) {
         <meta property="og:type" content="website" />
         <meta property="og:locale" content={props.locale} />
         <meta property="og:site_name" content="indays.app" />
-        <meta property="og:image" content="/indays-logo.png" />
-        <meta property="og:url" content="https://indays.app" />
-        <meta property="og:title" content={strings.page.title} />
-        <meta property="og:description" content={strings.page.description} />
 
         <meta property="twitter:card" content="summary" />
-        <meta property="twitter:image" content="/indays-logo.png" />
-        <meta property="twitter:title" content={strings.page.title} />
-        <meta property="twitter:description" content={strings.page.description} />
-
-        <link rel="canonical" href="https://indays.app/" />
-        <link rel="alternate" href="https://indays.app/en" hrefLang="en" />
-        <link rel="alternate" href="https://indays.app/" hrefLang="pt-BR" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
 
