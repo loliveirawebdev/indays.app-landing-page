391ec2e7f653f2c87a0741fe1d0ff233ae10c7c4
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-01-03 12:11:58 -0300

    fix: header & contact reference set when page changes

--- atoms/sectionRefBucket.ts
+++ atoms/sectionRefBucket.ts
@@ -3,14 +3,18 @@ import { atom, selector } from "recoil";
 const BASE_SECTION_REF_BUCKET = atom<App.SectionRef[]>({
   key: "baseSectionRefBucket",
   default: [],
+  dangerouslyAllowMutability: true,
 });
 
 export const SECTION_REF_BUCKET = selector<App.SectionRef[]>({
   key: "sectionRefBucket",
+  dangerouslyAllowMutability: true,
   get: ({ get }) => get(BASE_SECTION_REF_BUCKET),
-  set: ({ get, set }, newRef) => {
+  set: ({ get, set }, _newRef) => {
+    const newRef = _newRef as any[];
     const oldRefs = get(BASE_SECTION_REF_BUCKET);
-    const newRefs = [...oldRefs, ...(newRef as any)];
+    const filteredOldRefs = oldRefs.filter((ref) => ref.key !== newRef[0].key);
+    const newRefs = [...filteredOldRefs, ...(newRef as any)];
     set(BASE_SECTION_REF_BUCKET, newRefs);
   },
 });
