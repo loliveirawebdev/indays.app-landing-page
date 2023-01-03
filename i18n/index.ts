0b889a5575ec4b64caf5ee8d9743335ae5df194e
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-01-03 11:18:57 -0300

    feat: add blog localization definitions

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import HowWeWork from "./pages/HowWeWork";
+import Blog from "./pages/Blog";
 
 import Footer from "./blocks/Footer";
 import Contact from "./blocks/Contact";
@@ -8,11 +9,13 @@ import Header from "./blocks/Header";
 
 import HomeRoute from "./pages/Home/route.json";
 import HowWeWorkRoute from "./pages/HowWeWork/route.json";
+import BlogRoute from "./pages/Blog/route.json";
 
 export const LocalesMap = {
   home: Home as any,
   notFound: NotFound as any,
   howWeWork: HowWeWork as any,
+  blog: Blog as any,
   footer: Footer as any,
   contact: Contact as any,
   header: Header as any,
@@ -21,4 +24,5 @@ export const LocalesMap = {
 export const RoutesMap = {
   home: HomeRoute as any,
   howWeWork: HowWeWorkRoute as any,
+  blog: BlogRoute as any,
 };
