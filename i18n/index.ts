61fbf5a47cc0bdd8708e0edebffbeba1b5900309
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-01-03 21:01:00 -0300

    fix: remove useless files

--- i18n/index.ts
+++ i18n/index.ts
@@ -2,12 +2,6 @@
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import HowWeWork from "./pages/HowWeWork";
-import Blog from "./pages/Blog";
-
-// posts
-import LgesChecksForCheques from "./pages/LgesCheckForCheques";
-import ProjectPricing from "./pages/ProjectPricing";
-import CodeQuality from "./pages/CodeQuality";
 
 // blocks
 import Footer from "./blocks/Footer";
@@ -16,35 +10,21 @@ import Contact from "./blocks/Contact";
 
 // routes cfg
 import HomeRoute from "./pages/Home/route.json";
-import BlogRoute from "./pages/Blog/route.json";
 import HowWeWorkRoute from "./pages/HowWeWork/route.json";
-import LgesChecksForChequesRoute from "./pages/LgesCheckForCheques/route.json";
-import ProjectPricingRoute from "./pages/ProjectPricing/route.json";
-import CodeQualityRoute from "./pages/CodeQuality/route.json";
 
 export const LocalesMap = {
   // pages
   home: Home as any,
   notFound: NotFound as any,
   howWeWork: HowWeWork as any,
-  blog: Blog as any,
 
   // blocks
   footer: Footer as any,
   contact: Contact as any,
   header: Header as any,
-
-  // posts
-  lgesChecksForCheques: LgesChecksForCheques as any,
-  projectPricing: ProjectPricing as any,
-  codeQuality: CodeQuality as any,
 };
 
 export const RoutesMap = {
   home: HomeRoute as any,
   howWeWork: HowWeWorkRoute as any,
-  blog: BlogRoute as any,
-  lgesChecksForCheques: LgesChecksForChequesRoute as any,
-  projectPricing: ProjectPricingRoute as any,
-  codeQuality: CodeQualityRoute as any,
 };
