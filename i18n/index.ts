b797b0ae5cc66ee11ec1c701462b1e52e538a27c
Lucas Oliveira <lucas.oliveira@simbioseventures.com>
2023-02-02 23:39:41 -0300

    feat: add nitro10x page

--- i18n/index.ts
+++ i18n/index.ts
@@ -2,6 +2,7 @@
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import HowWeWork from "./pages/HowWeWork";
+import Nitro10x from "./pages/Nitro10x";
 
 // blocks
 import Footer from "./blocks/Footer";
@@ -11,12 +12,14 @@ import Contact from "./blocks/Contact";
 // routes cfg
 import HomeRoute from "./pages/Home/route.json";
 import HowWeWorkRoute from "./pages/HowWeWork/route.json";
+import Nitro10xRoute from "./pages/Nitro10x/route.json";
 
 export const LocalesMap = {
   // pages
   home: Home as any,
   notFound: NotFound as any,
   howWeWork: HowWeWork as any,
+  nitro10x: Nitro10x as any,
 
   // blocks
   footer: Footer as any,
@@ -27,4 +30,5 @@ export const LocalesMap = {
 export const RoutesMap = {
   home: HomeRoute as any,
   howWeWork: HowWeWorkRoute as any,
+  nitro10x: Nitro10xRoute as any,
 };
