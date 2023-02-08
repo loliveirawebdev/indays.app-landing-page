2bb85d7b8daee262cf58b07fdd02a1071479d427
Lucas Oliveira <lucas.oliveira@simbioseventures.com>
2023-02-08 18:38:04 -0300

    fix: change need help to open wpp

--- components/NeedHelp.tsx
+++ components/NeedHelp.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { useLocale } from "../hooks/locale";
 import { useNeedHelp } from "../hooks/needHelp";
-import { useSectionsScroll } from "../hooks/pageScroll";
 import style from "../styles/NeedHelp.module.scss";
 
 export const NeedHelp: React.FC = () => {
   const { strings } = useLocale("home");
   const { shouldBeVisible } = useNeedHelp();
-  const { scrollToContact } = useSectionsScroll();
 
   if (!shouldBeVisible) {
     return null;
   }
 
   return (
-    <button type="button" onClick={scrollToContact} itemScope itemProp="helper" className={style.contactUsHelper}>
+    <a
+      itemScope
+      type="button"
+      target="_blank"
+      itemProp="helper"
+      rel="noreferrer"
+      className={style.contactUsHelper}
+      href="https://wa.me/5519997494445"
+    >
       {strings.general.needHelp}
-    </button>
+    </a>
   );
 };
