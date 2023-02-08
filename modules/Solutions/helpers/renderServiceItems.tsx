ff465a74610bc56e23ce2699477baad09feba66d
Lucas Oliveira <lucas.oliveira@simbioseventures.com>
2023-02-08 11:46:35 -0300

    feat: solutions icons

--- modules/Solutions/helpers/renderServiceItems.tsx
+++ modules/Solutions/helpers/renderServiceItems.tsx
@@ -1,14 +1,24 @@
+import Image from "next/image";
 import { v4 as uuid } from "uuid";
 
-export const renderServiceItems = (items: string[]): React.ReactNode => {
+export const renderServiceItems = (items: any[]): React.ReactNode => {
   return items.map((item) => {
+    const { label, icon } = item;
     const uniqueKey = uuid();
 
     return (
       <li key={uniqueKey} itemProp="itemListElement" itemScope itemType="https://schema.org/Service">
-        <hgroup>
-          <h4 itemProp="name">{item}</h4>
-        </hgroup>
+        <Image
+          width={32}
+          alt={icon}
+          height={32}
+          quality={100}
+          loading="lazy"
+          itemProp="icon"
+          src={`/assets/solutions/${icon}.svg`}
+        />
+
+        <span>{label}</span>
       </li>
     );
   });
